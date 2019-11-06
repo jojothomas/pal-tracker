1482b4f3d431ffc8b818ae3012161442b52c4068
jojothomas <jojo@jojothomas.com>
2019-11-06 08:55:03 -0700

    Add JDBC Template

--- src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
+++ src/main/java/io/pivotal/pal/tracker/PalTrackerApplication.java
@@ -1,21 +1,30 @@
 package io.pivotal.pal.tracker;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import javax.sql.DataSource;
+
 @SpringBootApplication
 public class PalTrackerApplication {
 
+
+    private DataSource dataSource;
+
+    @Autowired
+    public PalTrackerApplication(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     public static void main(String[] args)
     {
         SpringApplication.run(PalTrackerApplication.class, args);
     }
 
-
     @Bean
     TimeEntryRepository timeEntryRepository() {
-        return new InMemoryTimeEntryRepository();
+        return new JdbcTimeEntryRepository(this.dataSource);
     }
 }
